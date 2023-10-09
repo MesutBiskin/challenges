3fb8a00d48b7d0de61eaa6bb4cae9697ccce1781
Mesut Biskin <msbiskin@gmail.com>
2023-10-09 16:58:15 -0400

    challenges

---
+++ index.js
@@ -0,0 +1,25 @@
+function hello(name){
+    const message = "Hello "
+    return message +name
+
+}
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+console.log(add(2,3))
+
+
+function multiply(num1, num2){
+    const result = num1 * num2
+    
+    return num1+'x' +num2 + '='+ result
+}
+
+console.log( multiply(2,3))
+
+
+
+
+// 2 X 3 = 6  ( Console da islemin sonucunu bu sekilde goster)
